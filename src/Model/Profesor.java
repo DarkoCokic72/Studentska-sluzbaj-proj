5f686c7d6a844589789e333978bcc7b4577405b9
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2021-11-13 19:26:51 +0100

    Professor Added

--- src/Model/Profesor.java
+++ src/Model/Profesor.java
@@ -1,7 +1,26 @@
 package Model;
 
+import java.sql.Date;
+import java.util.ArrayList;
 
-public class Profesor {
+public class Profesor extends Person {
+	
+	private int ID;
+	private String title;
+	private int yearsOfExperience;
+	private ArrayList<Subject> subjectsTeaches;
+	private Address officeAdress;
+	
+	public Profesor(String name, String surname, Date date, String eMail, String phone, Address address, Address officeAddress, int ID, String title, int yearsOfExperience) {
+		
+		super(name, surname, date, eMail, phone, address);
+		this.officeAdress = officeAddress;
+		this.ID = ID;
+		this.title = title;
+		this.yearsOfExperience = yearsOfExperience;
+		subjectsTeaches = new ArrayList<Subject>();
+				
+	}
 	
 	
 }
