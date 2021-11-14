03b4dfea828ed1b80b62e817646877cfe9cf3d60
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2021-11-14 14:07:08 +0100

    gui: Added MainFrame; main: changed MyApp

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,34 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+	
+public class MainFrame extends JFrame {
+		
+		public MainFrame() { 
+			
+		super(); 
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension d = kit.getScreenSize();
+		int width = d.width;
+		int height = d.height;
+		setSize(width*3/4, height*3/4); 
+		
+		setTitle("Studentska služba");
+		setLocationRelativeTo(null);
+		Image img=kit.getImage("images/icon.png");
+		setIconImage(img);
+		getContentPane().setBackground(Color.GRAY);
+		
+		setVisible(true);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		}
+}
+	
+	
