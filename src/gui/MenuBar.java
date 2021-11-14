5e86f1eed38269de008f9c72d946aeb251c41dbd
DarkoCokic72 <cokic.ra72.2019@uns.ac.rs>
2021-11-14 15:41:43 +0100

    Feature: added most of accelerators

--- src/gui/MenuBar.java
+++ src/gui/MenuBar.java
@@ -1,9 +1,13 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
 import javax.swing.ImageIcon;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
 public class MenuBar extends JMenuBar{
 	public MenuBar() {
@@ -28,6 +32,11 @@ public class MenuBar extends JMenuBar{
 		miOpen.setIcon(new ImageIcon("images/open-file-icon.png"));
 		miClose.setIcon(new ImageIcon("images/Close-2-icon.png"));
 		
+		miNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+		miSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+		miClose.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, ActionEvent.CTRL_MASK));
+		// FIXME miOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+		
 		file.add(miNew);
 		file.addSeparator();
 		file.add(miSave);
@@ -42,7 +51,14 @@ public class MenuBar extends JMenuBar{
 		JMenuItem miEdit = new JMenuItem("Edit");
 		JMenuItem miDelete = new JMenuItem("Delete");
 		
+		miEdit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+		miDelete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, ActionEvent.SHIFT_MASK));
+		
+		miEdit.setIcon(new ImageIcon("images/edit-icon.png"));
+		miDelete.setIcon(new ImageIcon("images/trash-icon.png"));
+		
 		edit.add(miEdit);
+		edit.addSeparator();
 		edit.add(miDelete);
 		
 		edit.setMnemonic('E');
@@ -51,7 +67,10 @@ public class MenuBar extends JMenuBar{
 		JMenuItem miHelp = new JMenuItem("Help");
 		JMenuItem miAbout = new JMenuItem("About");
 		
+		//FIXME miHelp.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, ActionEvent.KEY_EVENT_MASK));
+		
 		help.add(miHelp);
+		help.addSeparator();
 		help.add(miAbout);
 		
 		help.setMnemonic('H');
