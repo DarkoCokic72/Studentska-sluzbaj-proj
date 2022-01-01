78e798c6df07e8aa61cabe284a924fe32c88d6da
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2022-01-01 20:19:51 +0100

    Class SubjectJTable: Implemented search method

--- src/gui/SubjectJTable.java
+++ src/gui/SubjectJTable.java
@@ -1,12 +1,19 @@
 package gui;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.RowFilter;
+import javax.swing.table.TableRowSorter;
 
 public class SubjectJTable extends JTable {
 
 	 private SubjectAbstractTable subjectTableModel; 
-	 private static SubjectJTable subjectTable;
+	 private static SubjectJTable subjectTable = null;
+	 private TableRowSorter<SubjectAbstractTable> tableRowSorter;
+		
 	
      public SubjectJTable() {
 			
@@ -15,13 +22,35 @@ public class SubjectJTable extends JTable {
        	    setRowSelectionAllowed(true);
        	    setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     		getTableHeader().setReorderingAllowed(false);
-    		
     		setModel(subjectTableModel);
+    		tableRowSorter = new TableRowSorter<SubjectAbstractTable>(subjectTableModel);
+    		setRowSorter(tableRowSorter);
+    		
      }
      
-     public void search(String text) {
-     	
+     public void updateTable() {
+     	subjectTableModel.fireTableDataChanged();
      }
+     
+     public void search(String input) {
+    	 
+    	String[] words = input.split(" ");
+      	int numberOfWords = words.length;
+      	if (numberOfWords == 1) {
+      		RowFilter<SubjectAbstractTable, Integer> filter1 = RowFilter.regexFilter(".*" + "(?i)" + words[0] + ".*");
+      		List<RowFilter<SubjectAbstractTable, Integer>> filters = Arrays.asList(filter1);
+      		tableRowSorter.setRowFilter(RowFilter.andFilter(filters));
+      	}
+      	if (numberOfWords == 2) {
+      		
+      		RowFilter<SubjectAbstractTable, Integer> filter1 = RowFilter.regexFilter(".*" + "(?i)" + words[0] + ".*");
+      		RowFilter<SubjectAbstractTable, Integer> filter2 = RowFilter.regexFilter(".*" + "(?i)" + words[1] + ".*");
+      		List<RowFilter<SubjectAbstractTable, Integer>> filters = Arrays.asList(filter1, filter2);
+      		tableRowSorter.setRowFilter(RowFilter.andFilter(filters));
+      	} 	
+    	 
+     }
+     	
      
      public static SubjectJTable getTable() {
  		if (subjectTable == null) {
--- src/main/MyApp.java
+++ src/main/MyApp.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import Controller.Converter;
 import Model.Address;
 import Model.Profesor;
+import Model.ProfessorDatabase;
 import Model.Profesor.Zvanje;
 import Model.Student;
 import Model.Student.Status;
@@ -13,7 +14,9 @@ import Model.SubjectDatabase;
 import Model.StudentDatabase;
 import Model.Subject;
 import gui.MainFrame;
+import gui.ProfessorJTable;
 import gui.StudentTable;
+import gui.SubjectJTable;
 
 public class MyApp {
 
@@ -21,28 +24,6 @@ public class MyApp {
 		
 		MainFrame mf = MainFrame.getMainFrame();
 		
-		Student s = new Student("Ana", "Vulin", Converter.convertStringToDate("4.3.2000"), "sm.vulinana@gmail.com", "0690386028", 
-				new Address("Rumenacka", 17, "Novi Sad", "Srbija"), "ra-65-2019", 2019, 1, Status.B);
-		StudentDatabase studDatabase = StudentDatabase.getInstance();
-		studDatabase.addStudentsIntoTable(s);
-		StudentTable studTable = StudentTable.getTable();
-		studTable.updateTable();
-		
-		Subject subj = new Subject("S101", " OISISI", Term.WINTER,
-				3, new Profesor("Marko", "Markovic", Converter.convertStringToDate("1.1.1990."), "marko@gmail.com", "0611123526", 
-						new Address("Rumenacka", 17, "Novi Sad", "Srbija"), new Address("Rumenacka", 17, "Novi Sad", "Srbija"), 123456789, 10, Zvanje.REDOVNI_PROFESOR), 5);
-		ArrayList<Subject> unpassedCourses= new ArrayList<Subject>();
-		unpassedCourses.add(subj);
-		SubjectDatabase subjectDatabase = SubjectDatabase.getDatabase();
-		subjectDatabase.addSubjectIntoTable(subj);
-		
-		
-		Subject subj2 = new Subject("S102", "NANS", Term.WINTER,
-				3, new Profesor("Marko", "Markovic", Converter.convertStringToDate("1.1.1990."), "marko@gmail.com", "0611123526", 
-						new Address("Rumenacka", 17, "Novi Sad", "Srbija"), new Address("Rumenacka", 17, "Novi Sad", "Srbija"), 123456789, 10, Zvanje.REDOVNI_PROFESOR), 5);
-		unpassedCourses.add(subj2);
-		subjectDatabase.addSubjectIntoTable(subj2);
-		s.setUnpassedCourses(unpassedCourses);
 	}
 
 }
