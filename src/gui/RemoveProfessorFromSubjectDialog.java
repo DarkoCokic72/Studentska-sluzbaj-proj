935009c1d6a12aa9d0096abdbd26a1c4f87bd0b3
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2022-01-09 01:58:50 +0100

    Added RemoveProfessorFromSubjectDialog

---
+++ src/gui/RemoveProfessorFromSubjectDialog.java
@@ -0,0 +1,83 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class RemoveProfessorFromSubjectDialog extends JDialog{
+	
+	JPanel northPanel;
+	JPanel southPanel;
+	private static JLabel areYouSureLab;
+	private static JButton yes;
+	private static JButton no ;
+		
+	public RemoveProfessorFromSubjectDialog() {
+		northPanel = new JPanel();
+		southPanel = new JPanel();
+		
+		setVisible(true);
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = d.width;
+		int height = d.height;
+		setSize(width*1/4, height*1/5);
+		ImageIcon img = new ImageIcon("images/Recycle-Bin-icon.png");
+		setIconImage(img.getImage());
+		setTitle("Ukloni profesora");
+		setResizable(false);
+		
+		areYouSureLab = new JLabel();
+		areYouSureLab.setText("Da li ste sigurni?");
+		northPanel.add(areYouSureLab);
+		
+		yes = new JButton("Potvrdi");
+		yes.setPreferredSize(new Dimension(100,30));
+		southPanel.add(yes);
+		
+		yes.addActionListener(new ActionListener() {
+	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				
+		}
+	
+});
+		
+		southPanel.add(Box.createRigidArea(new Dimension(30, 0)));
+		
+		no = new JButton("Odustani");
+		no.setPreferredSize(new Dimension(100,30));
+		southPanel.add(no);
+		
+		no.addActionListener(new ActionListener() {
+	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				dispose();
+				
+			}
+	
+		});
+		
+		northPanel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+		add(northPanel,BorderLayout.CENTER);
+		southPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		add(southPanel, BorderLayout.SOUTH);
+		
+		revalidate(); 
+		repaint();
+	}
+
+}
